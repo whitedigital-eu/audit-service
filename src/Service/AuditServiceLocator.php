66c8caa12c5543acd453fdc45f0b2fcbd68bab75
Davis Zalitis <davis.zalitis@whitedigital.eu>
2023-01-05 15:38:21 +0200

    fix default class

--- src/Service/AuditServiceLocator.php
+++ src/Service/AuditServiceLocator.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\Attribute\TaggedLocator;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 use Throwable;
 use WhiteDigital\Audit\Contracts\AuditServiceInterface;
+use WhiteDigital\Audit\Entity\Audit;
 
 use function array_key_first;
 
@@ -18,12 +19,12 @@ class AuditServiceLocator implements AuditServiceInterface
         $this->audit = $audits->get(array_key_first($audits->getProvidedServices()));
     }
 
-    public function audit(string $type, string $message, array $data = [], string $class = ''): void
+    public function audit(string $type, string $message, array $data = [], string $class = Audit::class): void
     {
         $this->audit->audit($type, $message, $data, $class);
     }
 
-    public function auditException(Throwable $exception, ?string $url = null, string $class = ''): void
+    public function auditException(Throwable $exception, ?string $url = null, string $class = Audit::class): void
     {
         $this->audit->auditException($exception, $url, $class);
     }
